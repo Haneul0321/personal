02ec1116d46ec712aa7e1df08aada330efbf7447
kangsky <skykang98@gmail.com>
2025-05-14 11:20:09 +0900

    update properties

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,5 +1,13 @@
 package app.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmpAPIController {
 
+	@GetMapping("/api/emp-test")
+	public String emptest() {
+		return "emp api controller";
+	}
 }
